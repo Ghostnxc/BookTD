66584c8ac9b3faae3785301d1efc33c0c6a2cfb9
Ghostnxc <Phantomnxc@gmail.com>
2020-12-10 17:21:11 -0500

    chapeter 5 first commit

--- functional_test.py
+++ functional_test.py
@@ -35,27 +35,41 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         #"1: Buy peacock fethers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(10)
+        time.sleep(1)
+   #     self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not apper in table"
-        )
+    
+
+
+      
+        
         #There is still a text box inviting her to add another item. She
         # enters "Use peacock fethers to make a fly" (Edith is very 
         # methodical)
-        self.fail('finsih the test')
+     
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock fethers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
 
         #The page updates again, and now shows both items on her list
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        self.assertTrue(
+            self.assertIn('2: Use peacock feathers to make a fly',
+            [row.text for row in rows])
+            
+        )
+  
         #Edith wonders whether he site will remember her list. Then she sees
         # that the site has a generated a unique URL for her -- there is some
-        # explantory rexr that effect.
-
-        #She cisits that URL - her to-do list is still there.
-
+        # explantory texr that effect.
+        self.fail('Finish the test!')
+        #She visits that URL - her to-do list is still there.
+        
         #Satisfied, she foes back to sleep
 if __name__=='__main__':
         unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,31 @@
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
+from django.http import HttpRequest, response
 
 from lists.views import home_page
 
 class HomePageTest(TestCase):
-    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+
+
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  
-        response = home_page(request)  
+    def test_home_page_returns_correct_html(self):
+
+        response = self.client.get('/')  
+
         html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))  
-        self.assertIn('<title>To-Do lists</title>', html)  
-        self.assertTrue(html.endswith('</html>'))  
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')  
+    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
