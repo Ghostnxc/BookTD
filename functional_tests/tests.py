c1bd7900e9daf3eb91bb3835a4a5c2ffb5781fc7
Ghostnxc <Phantomnxc@gmail.com>
2020-12-10 22:06:41 -0500

    chapeter 6 first commit

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
     
@@ -18,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Edith heard about a cool online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         #She notices the page title and header mention to-do lists
         self.assertIn('To-Do' , self.browser.title)
@@ -35,13 +37,13 @@ class NewVisitorTest(unittest.TestCase):
 
         #She types "Buy peacock fethers" into a text box (Edith's hobby
         # is trying fly-fishing lures)
-        inputbox.send_keys("Buy peacock fethers")
+        inputbox.send_keys("Buy peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists
         #"1: Buy peacock fethers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock fethers')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         
         #There is still a text box inviting her to add another item. She
@@ -49,7 +51,7 @@ class NewVisitorTest(unittest.TestCase):
         # methodical)
      
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock fethers to make a fly')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
